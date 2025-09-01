05a0d727a5517a47875059b9be0b69d6dc5f5fa2
DESKTOP-QCFO6Q2\NIKESH <nekesmgr@gmail.com>
2025-09-01 13:38:24 +0545

    changes on ResponseTrait file

--- app/ResponseTrait.php
+++ app/ResponseTrait.php
@@ -21,27 +21,4 @@ trait ResponseTrait
             'success' => false,
         ], $statusCode);
     }
-    function colorNameToHex(string $colorName): ?string
-    {
-        $colors = [
-            'black' => '#000000',
-            'white' => '#FFFFFF',
-            'red' => '#FF0000',
-            'green' => '#008000',
-            'blue' => '#0000FF',
-            'yellow' => '#FFFF00',
-            'cyan' => '#00FFFF',
-            'magenta' => '#FF00FF',
-            'gray' => '#808080',
-            'orange' => '#FFA500',
-            'pink' => '#FFC0CB',
-            'purple' => '#800080',
-            'brown' => '#A52A2A',
-            'skyblue' => '#87CEEB',
-            // Add more as needed
-        ];
-
-        $key = strtolower(trim($colorName));
-        return $colors[$key] ?? null; // returns null if color not found
-    }
 }
