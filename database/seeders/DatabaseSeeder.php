d0f7d31b6e41f70012caf4ae6c88d9f79e821956
DESKTOP-QCFO6Q2\NIKESH <nekesmgr@gmail.com>
2025-09-01 22:37:15 +0545

    DatabaseSeeder file updated(seeds only users and categories)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,7 +16,29 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            ProductSeeder::class,
+            // ProductSeeder::class,
+        ]);
+        DB::table('categories')->insert([
+            [
+                'title' => 'Tablets/Capsules',
+                'slug' => 'tablets-capsules',
+            ],
+            [
+                'title' => 'Syrup/Suspension',
+                'slug' => 'syrup-suspension',
+            ],
+            [
+                'title' => 'Injection',
+                'slug' => 'injection',
+            ],
+            [
+                'title' => 'Cream/Ointment',
+                'slug' => 'cream-ointment',
+            ],
+            [
+                'title' => 'Drops',
+                'slug' => 'drops',
+            ]
         ]);
     }
 }
