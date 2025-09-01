6bcdf78886888623b1750850cd7e21856338c8da
DESKTOP-QCFO6Q2\NIKESH <nekesmgr@gmail.com>
2025-09-01 15:22:36 +0545

    swagger config setup file

---
+++ app/Http/Controllers/Api/V1/Admin/AdminController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+/**
+ *  @OA\Info(
+ *     version="1.0",
+ *     title="Medishop",
+ *     description="Medishop Project"
+ * )
+ *
+ *  @OA\Server(
+ *     url="http://127.0.0.1:8000/api/v1",
+ *     description="Localhost API Server"
+ * )
+ * @OA\Server(
+ *     url="http://nikeshthapa.myartsonline.com/",
+ *     description="Live API Server"
+ * )
+ */
+class AdminController extends Controller
+{
+}
