74e6d9f2623b8250e9af93bd225301850dc05bd5
DESKTOP-QCFO6Q2\NIKESH <nekesmgr@gmail.com>
2025-09-02 22:29:26 +0545

    removed unused routes

--- routes/api.php
+++ routes/api.php
@@ -17,46 +17,6 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::prefix('v1')->group(function(){
-    //client
     require __DIR__ . '/auth.php';
     require __DIR__ . '/admin.php';
-    Route::controller(MainController::class)->group(function () {
-        Route::get('/product', 'product');
-        Route::get('/categories', 'categories');
-        Route::get('/product-detail/{slug}', 'product_detail');
-        Route::get('/banner', 'banner');
-    });
-
-    Route::controller(CartController::class)->group(function () {
-        Route::get('/view-cart', 'view_cart')->middleware('auth:sanctum');
-        Route::post('/add-to-cart', 'add_cart')->middleware('auth:sanctum');
-        Route::post('/update-cart/{cart}', 'update_cart')->middleware('auth:sanctum');
-        Route::delete('/delete-cart/{cart}', 'delete_from_cart')->middleware('auth:sanctum');
-    });
-
-    //Admin section
-    Route::prefix('admin')->group(function () {
-        Route::middleware(['auth:sanctum', 'verified', AdminMiddleware::class])->group(function () {
-            Route::controller(ProductController::class)->group(function () {
-                Route::post('/add-product', 'add_product');
-                Route::post('/update-product/{product}', 'update_product');
-                Route::delete('/delete-product/{product}', 'delete_product');
-                Route::post('/restore-product/{id}', 'restore_product');
-            });
-            Route::controller(BannerController::class)->group(function () {
-                Route::post('/add-banner', 'add_banner');
-                Route::post('/update-banner/{banner}', 'update_banner');
-                Route::delete('/delete-banner/{banner}', 'delete_banner');
-            });
-            Route::controller(CategoriesController::class)->group(function () {
-                Route::post('/add-categories', 'add_categories');
-                Route::post('/update-categories/{categories}', 'update_categories');
-                Route::delete('/delete-categories/{category}', 'delete_categories');
-            });
-            Route::controller(UserController::class)->group(function () {
-                Route::get('/view-user', 'View_User');
-                Route::delete('/delete-user/{user}', 'delete');
-            });
-        });
-    });
 });
