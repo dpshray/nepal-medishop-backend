70a1844ea5edbb2614e747a091cb091a485bd41b
DESKTOP-QCFO6Q2\NIKESH <nekesmgr@gmail.com>
2025-09-05 17:57:02 +0545

    added additional url on swagger

--- app/Http/Controllers/Api/V1/Admin/AdminController.php
+++ app/Http/Controllers/Api/V1/Admin/AdminController.php
@@ -12,11 +12,21 @@ use Illuminate\Http\Request;
  *     description="Medishop Project"
  * )
  *
+ * @OA\Server(
+ *     url="https://api.medishop.dworklabs.com/api/v1",
+ *     description="Live API Server"
+ * )
+ *  
  *  @OA\Server(
  *     url="http://192.168.100.23:8008/api/v1",
  *     description="Localhost API Server 1"
  * )
-*/
+ * 
+ * @OA\Server(
+ *     url="http://127.0.0.1:8000/api/v1",
+ *     description="Localhost API Server 2"
+ * )
+ */
 class AdminController extends Controller
 {
 }
--- app/Http/Controllers/Api/V1/Client/ClientController.php
+++ app/Http/Controllers/Api/V1/Client/ClientController.php
@@ -11,10 +11,20 @@ use Illuminate\Http\Request;
  *     title="Medishop",
  *     description="Medishop Project"
  * )
+ * 
+ * @OA\Server(
+ *     url="https://api.medishop.dworklabs.com/api/v1",
+ *     description="Live API Server"
+ * )
  *
- *  @OA\Server(
+ * @OA\Server(
  *     url="http://192.168.100.23:8008/api/v1",
  *     description="Localhost API Server 1"
+ * )
+ * 
+ * @OA\Server(
+ *     url="http://127.0.0.1:8000/api/v1",
+ *     description="Localhost API Server 2"
  * )
  */
 class ClientController extends Controller
