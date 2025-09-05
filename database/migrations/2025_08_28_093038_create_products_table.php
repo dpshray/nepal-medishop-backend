36412e96cd774c23412b8233c30ca8dfe712c938
DESKTOP-QCFO6Q2\NIKESH <nekesmgr@gmail.com>
2025-09-05 13:05:01 +0545

    satatus column type changed bool -> string

--- database/migrations/2025_08_28_093038_create_products_table.php
+++ database/migrations/2025_08_28_093038_create_products_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->uuid('uuid');
-            $table->boolean('status')->default(true);
+            $table->string('status');
             $table->foreignId('category_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
             $table->string('slug')->unique();
             $table->string('name');
