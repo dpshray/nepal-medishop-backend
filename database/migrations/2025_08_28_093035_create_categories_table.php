143a695b92c166e1ce15ec35d383df91671f856b
DESKTOP-QCFO6Q2\NIKESH <nekesmgr@gmail.com>
2025-09-09 16:32:00 +0545

    category column name changed

--- database/migrations/2025_08_28_093035_create_categories_table.php
+++ database/migrations/2025_08_28_093035_create_categories_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('slug')->unique();
-            $table->string('title')->unique();
+            $table->string('name')->unique();
             $table->softDeletes();
         });
     }
