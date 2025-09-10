c38f0694585c934f3d7f1924dde778793a4639da
DESKTOP-QCFO6Q2\NIKESH <nekesmgr@gmail.com>
2025-09-10 11:05:52 +0545

    minor changes

--- routes/auth.php
+++ routes/auth.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(ClientAuthController::class)->group(function () {
     Route::post('/login', 'login');
-    Route::post('/logout', 'logout')->middleware(['auth:sanctum']);
+    Route::middleware(['auth:sanctum'])->post('/logout', 'logout');
     Route::post('/register', 'register');
     Route::get('verification/verify', 'emailVerifier')->name('verification.verify')->middleware(['signed']);
     Route::post('forgot-password', 'sendPasswordResetLink');
