9f4dac5bdc8b5be060e024c783e91928ee076f14
DESKTOP-QCFO6Q2\NIKESH <nekesmgr@gmail.com>
2025-09-18 15:53:53 +0545

    migration file changed

--- database/migrations/2025_08_28_093038_create_products_table.php
+++ database/migrations/2025_08_28_093038_create_products_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->string('slug')->unique();
             $table->string('name');
             $table->text('description');
-            $table->decimal('rating', 2, 1);
+            $table->decimal('rating', 2, 1)->default(0);
             $table->timestamps();
             $table->softDeletes();
         });
