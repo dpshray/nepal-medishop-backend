ed24044c62b42c30b7d80e0194e108d6616dc5bf
DESKTOP-QCFO6Q2\NIKESH <nekesmgr@gmail.com>
2025-09-18 16:48:32 +0545

    resource file

--- app/Http/Resources/User/Product/Card/ProductCardResource.php
+++ app/Http/Resources/User/Product/Card/ProductCardResource.php
@@ -16,16 +16,17 @@ class ProductCardResource extends JsonResource
     public function toArray(Request $request): array
     {
         // return parent::toArray($request);
-        $item_price = $this->vendorPrices->sortByDesc('platform_price')->first();
+        $item_price = $this->cheapestVariation;
+        // dd($this->product);
         $platform_price = $item_price->platform_price;
         $discount_price = null;
-        if ($item_price->discount_price) {
-            $discount_percent = (($item_price->price - $item_price->discount_price)/ $item_price->price) * 100;
+        if ($item_price->platform_discount_price) {
+            $discount_percent = (($item_price->price - $item_price->platform_discount_price)/ $item_price->price) * 100;
             $discount_price = round($platform_price + ($discount_percent * $platform_price)/100, 2);  
         }
         return [
-            'name' => $this->whenLoaded('product', $this->product->name),
-            'brand' => new CardBrandResource($this->whenLoaded('brand')),
+            'name' => $this->name,
+            'brand' => $this->whenLoaded('brand', fn() => $this->brand->name),
             'rating' => (float) $this->rating,
             'price' => (float) $platform_price,
             'previous_price' => $discount_price,
