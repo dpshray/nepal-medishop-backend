68d8e5cf6a1fcd43b7d9310b328fd224b360c287
DESKTOP-QCFO6Q2\NIKESH <nekesmgr@gmail.com>
2025-09-23 12:30:27 +0545

    if(has_discount_price) previous_price = price and price = discount_price

--- app/Http/Resources/User/Product/Card/ProductCardResource.php
+++ app/Http/Resources/User/Product/Card/ProductCardResource.php
@@ -17,20 +17,19 @@ class ProductCardResource extends JsonResource
     {
         // return parent::toArray($request);
         $item = $this->cheapestVariation;
-        // dd($this->product);
         $platform_price = $item->platform_price;
-        $discount_price = null;
+        $previous_price = null;
         if ($item->platform_discount_price) {
-            $discount_percent = (($platform_price - $item->platform_discount_price)/ $platform_price) * 100;
-            $discount_price = round($platform_price + ($discount_percent * $platform_price)/100, 2);  
+            $previous_price = (float) $platform_price;
+            $platform_price = $item->platform_discount_price;
         }
         return [
             'name' => $this->name,
             'brand' => $this->whenLoaded('brand', fn() => $this->brand->name),
             'rating' => (float) $this->rating,
             'price' => (float) $platform_price,
-            'previous_price' => $discount_price,
-            'feature_image' => $this->whenLoaded('product', fn() => $this->product->getFirstMediaUrl(Product::PRODUCT_FEATURE))
+            'previous_price' => $previous_price,
+            'feature_image' => $this->whenLoaded('media', fn() => $this->getFirstMediaUrl(Product::PRODUCT_FEATURE))
         ];
     }
 }
