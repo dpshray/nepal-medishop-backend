fcf1456dc338233d52532cb6ac1699bb05c415f4
DESKTOP-QCFO6Q2\NIKESH <nekesmgr@gmail.com>
2025-09-24 17:01:54 +0545

    pagination trait code updated

--- app/Traits/PaginationTrait.php
+++ app/Traits/PaginationTrait.php
@@ -9,6 +9,20 @@ trait PaginationTrait
 
     public array $data = [];
 
+    private $page = 'page';
+    private $items = 'items';
+    private $total_page = 'page';
+    private $total_items = 'total_items';
+
+
+    public function setDataKey(array $keys){
+        $this->page = array_key_exists($this->page, $keys) ? $keys[$this->page] : $this->page;
+        $this->items = array_key_exists($this->items, $keys) ? $keys[$this->items] : $this->items;
+        $this->total_page = array_key_exists($this->total_page, $keys) ? $keys[$this->total_page] : $this->total_page;
+        $this->total_items = array_key_exists($this->total_items, $keys) ? $keys[$this->total_items] : $this->total_items;
+        return $this;
+    }
+
     public function makePaginationResponse(LengthAwarePaginator $pagination, callable $call, array $append = []): self
     {
         $items = $pagination->items();
@@ -16,7 +30,7 @@ trait PaginationTrait
         $items = $call($items);
 
         $this->data['items'] = $items;
-        $this->data['page'] = $pagination->currentPage();
+        $this->data[$this->page] = $pagination->currentPage();
         $this->data['total_page']    = $pagination->lastPage();
         $this->data['total_items']        = $pagination->total();
 
