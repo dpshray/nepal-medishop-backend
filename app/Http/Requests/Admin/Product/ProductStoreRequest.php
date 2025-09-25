a145b3aecf54f1739555b0db41b25b00e4373dcc
DESKTOP-QCFO6Q2\NIKESH <nekesmgr@gmail.com>
2025-09-25 16:53:38 +0545

    changes based on(prescription_required and variant name)

--- app/Http/Requests/Admin/Product/ProductStoreRequest.php
+++ app/Http/Requests/Admin/Product/ProductStoreRequest.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Requests\Admin\Product;
 
+use App\Enums\ProductUnitEnum;
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\Rule;
 
 class ProductStoreRequest extends FormRequest
 {
@@ -35,10 +36,12 @@ class ProductStoreRequest extends FormRequest
             'tags' => 'required|array',
             'tags.*' => 'distinct|exists:tags,id',
             'variations' => 'required|array',
+            'variations.*.name' => 'required|string',
             'variations.*.size_value' => 'required|numeric',
-            'variations.*.size_unit' => 'required|string',
+            'variations.*.size_unit' => ['required', Rule::in(array_column(ProductUnitEnum::cases(), 'value'))],
             'variations.*.platform_price' => 'required|numeric',
             'variations.*.platform_discount_price' => 'sometimes|nullable|numeric',
+            'prescription_required' => 'sometimes'
         ];
         if ($product_id == null) {
             $rules = array_merge($rules, [
@@ -55,9 +58,11 @@ class ProductStoreRequest extends FormRequest
             'categories.*.distinct' => 'Categories has a duplicate values.',
             'tags.*.distinct' => 'Tags has a duplicate values.',
             'variations.required' => 'variation is required.',
-            'variations.*.size_value.required' => 'size value is required.',
-            'variations.*.size_unit.required' => 'size unit is required.',
-            'variations.*.platform_price.required' => 'platform price is required.',
+            'variations.*.name.required' => 'Variant name is required.',
+            'variations.*.size_value.required' => 'Variant size value is required.',
+            'variations.*.size_unit.required' => 'Variant size unit is required.',
+            'variations.*.size_unit.in' => 'Variant size unit must be one of: ' . implode(', ', array_column(ProductUnitEnum::cases(), 'value')),
+            'variations.*.platform_price.required' => 'Variant platform price is required.',
         ];
     }
 
@@ -67,6 +72,10 @@ class ProductStoreRequest extends FormRequest
             return is_array($item) ? $item : json_decode($item, true);
         }, $this->variations);
         $this->merge(['variations' => $converted]);
+
+        $this->merge([
+            'prescription_required' => filter_var($this->prescription_required, FILTER_VALIDATE_BOOLEAN) ? 1 : 0
+        ]);
         
     }
 }
