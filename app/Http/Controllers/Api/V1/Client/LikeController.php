1b458f0b0a9babdcefec47a3a0efced544a264fa
DESKTOP-QCFO6Q2\NIKESH <nekesmgr@gmail.com>
2025-10-07 13:44:26 +0545

    removed package favourate api

--- app/Http/Controllers/Api/V1/Client/LikeController.php
+++ app/Http/Controllers/Api/V1/Client/LikeController.php
@@ -52,46 +52,4 @@ class LikeController extends Controller
         }
         return $this->apiSuccess($msg);
     }
-
-    /**
-     * @OA\Get(
-     *     security={{"sanctum": {}}},
-     *     path="/favourite/{slug}/package",
-     *     summary="Toggle a favourite status of a package",
-     *     description="Toggle a favourite status of a package.",
-     *     operationId="PackageFavoutiteToggle",
-     *     tags={"Favourite"},
-     *     @OA\Parameter(
-     *         name="slug",
-     *         in="path",
-     *         required=true,
-     *         description="Slug of package",
-     *         @OA\Schema(type="string", example="super-box")
-     *     ),
-     *     @OA\Response(
-     *         response=200,
-     *         description="Successful toggle",
-     *         @OA\JsonContent(
-     *             type="object",
-     *             @OA\Property(property="message", type="string", example="Item added to favourite"),
-     *             @OA\Property(property="data", type="string", nullable=true, example=null),
-     *             @OA\Property(property="success", type="boolean", example=true)
-     *         )
-     *     )
-     * )
-     */
-    function togglePackageFavourite(Package $package){
-        $query = $package->likes();
-        $user_like = $query->where('user_id', Auth::id());
-        $msg = null;
-        if ($user_like->exists()) {
-            $user_like->delete();
-            $msg = 'Item removed from favourite';
-        } else {
-            $msg = 'Item added to favourite';
-            $query->create(['user_id' => Auth::id()]);
-        }
-        return $this->apiSuccess($msg);
-    }
-
 }
