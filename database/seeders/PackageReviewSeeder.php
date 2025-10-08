f0950c29e4d655357d47de686940c51e0f7d31c9
DESKTOP-QCFO6Q2\NIKESH <nekesmgr@gmail.com>
2025-10-08 16:05:55 +0545

    package seeder added

---
+++ database/seeders/PackageReviewSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enums\UserTypeEnum;
+use App\Models\Package;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PackageReviewSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $half_packages = (int) DB::table('packages')->count() / 2;
+        $user_ids = User::where('user_type', UserTypeEnum::USER->value)->take(25)->pluck('id')->all();
+        DB::transaction(function () use ($half_packages, $user_ids) {
+            Package::active()->take($half_packages)->each(function ($package) use ($user_ids) {
+                $temp = [];
+                foreach ($user_ids as $user_id) {
+                    $created_at = fake()->dateTimeBetween('-7 days', '-1 days');
+                    $temp[] = [
+                        'user_id' => $user_id,
+                        'review' => fake()->realText(fake()->numberBetween(200, 500)),
+                        'rating' => rand(1, 5),
+                        'created_at' => $created_at,
+                        'updated_at' => fake()->boolean(20) ? now() : $created_at
+                    ];
+                }
+                $package->reviews()->createMany($temp);
+            });
+        });
+    }
+}
