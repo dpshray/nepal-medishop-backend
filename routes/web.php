50ffd28c9ab3102928b186c6bc474c89ff3ae4c5
DESKTOP-QCFO6Q2\NIKESH <nekesmgr@gmail.com>
2025-10-16 16:41:45 +0545

    back to original state

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,7 @@
 <?php
 
-use App\Models\Purchase\Order;
-use App\Models\Purchase\OrderItem;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::get('test', function(){
-    $rows = Order::find(8)->orderItems()->with(['product.variations', 'package'])->get();
-    dd($rows);
 });
