86ff1a12994f69f84a967c86d0b3d1556a5011e3
DESKTOP-QCFO6Q2\NIKESH <nekesmgr@gmail.com>
2025-10-17 11:56:56 +0545

    brand destroy bug handle

--- app/Http/Controllers/Api/V1/Admin/Product/AdminBrandController.php
+++ app/Http/Controllers/Api/V1/Admin/Product/AdminBrandController.php
@@ -288,7 +288,7 @@ class AdminBrandController extends Controller
     */
     public function destroy(Brand $brand)
     {
-        if ($brand->has('products')) {
+        if ($brand->products()->exists()) {
             return $this->apiError("Deletion failed: This brand has associated products. Delete those products or change their brand before trying again.", 409);
         }
         $brand->delete();
