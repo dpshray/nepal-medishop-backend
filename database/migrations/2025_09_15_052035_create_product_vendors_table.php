b1ec200269078cda161e176be8e762da76ced0ee
DESKTOP-QCFO6Q2\NIKESH <nekesmgr@gmail.com>
2025-11-15 22:12:21 +0545

    migration changes

--- database/migrations/2025_09_15_052035_create_product_vendors_table.php
+++ database/migrations/2025_09_15_052035_create_product_vendors_table.php
@@ -13,12 +13,12 @@ return new class extends Migration
     {
         Schema::create('product_vendors', function (Blueprint $table) {
             $table->id();
+            $table->uuid('uuid');
             $table->boolean('status')->default(1)->comment('Vendor control: 1 = visible to admin, 0 = hidden by vendor');
             $table->boolean('is_approved')->default(0)->comment('Admin approval status: 1 = approved by admin (vendor’s marked price validated), 0 = not yet approved');
             $table->unsignedBigInteger('vendor_id');
             $table->foreign('vendor_id')->references('id')->on('vendors')->cascadeOnDelete()->cascadeOnUpdate();
             $table->foreignId('product_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
-            $table->boolean('is_featured')->default(0);
             $table->softDeletes();
         });
     }
