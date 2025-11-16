407f7bc0dfcd0bdf909a468c160c782bf49ac6aa
DESKTOP-QCFO6Q2\NIKESH <nekesmgr@gmail.com>
2025-11-16 21:49:42 +0545

    removed status from vendor product table

--- database/migrations/2025_09_15_055529_create_vendor_product_prices_table.php
+++ database/migrations/2025_09_15_055529_create_vendor_product_prices_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('vendor_product_prices', function (Blueprint $table) {
             $table->id();
-            $table->boolean('status')->default(true);
             $table->unsignedBigInteger('product_vendor_id');
             $table->foreign('product_vendor_id')->references('id')->on('product_vendors')->cascadeOnDelete()->cascadeOnUpdate();
             $table->foreignId('product_variation_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
