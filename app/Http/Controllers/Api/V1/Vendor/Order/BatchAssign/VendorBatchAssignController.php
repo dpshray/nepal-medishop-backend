f4ac24f77c876addd4bac5ea4207cafb13118b6d
Soyamstha <shrestha.soyam111@gmail.com>
2025-11-21 10:46:28 +0545

    migration change

---
+++ app/Http/Controllers/Api/V1/Vendor/Order/BatchAssign/VendorBatchAssignController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Vendor\Order\BatchAssign;
+
+use App\Http\Controllers\Controller;
+use App\Models\Purchase\OrderItem;
+use Illuminate\Http\Request;
+
+class VendorBatchAssignController extends Controller
+{
+    //
+    function assignbatch(OrderItem $orderItem,Request $request)
+    {
+
+    }
+}
