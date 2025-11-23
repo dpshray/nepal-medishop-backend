f0c3e32fb3d9e94822c12d4c16492bb3a33c4aba
DESKTOP-QCFO6Q2\NIKESH <nekesmgr@gmail.com>
2025-11-23 14:52:11 +0545

    added variation relation on OrderItemProduct model

--- app/Models/Purchase/OrderItemProduct.php
+++ app/Models/Purchase/OrderItemProduct.php
@@ -2,11 +2,16 @@
 
 namespace App\Models\Purchase;
 
+use App\Models\ProductVariation;
 use Illuminate\Database\Eloquent\Model;
 
 class OrderItemProduct extends Model
 {
     function orderItem() {
         return $this->belongsTo(OrderItem::class);
+    }
+
+    function variation() {
+        return $this->belongsTo(ProductVariation::class,'product_variation_id');
     }
 }
