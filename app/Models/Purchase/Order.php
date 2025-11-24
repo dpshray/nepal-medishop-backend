ec1167ef4ee41065e638eef78494ac9d88ee8dad
DESKTOP-QCFO6Q2\NIKESH <nekesmgr@gmail.com>
2025-11-24 09:10:09 +0545

    readded loyality point part on model

--- app/Models/Purchase/Order.php
+++ app/Models/Purchase/Order.php
@@ -2,6 +2,10 @@
 
 namespace App\Models\Purchase;
 
+use App\Enums\LoyalityPoint\LoyalityPointSourceEnum;
+use App\Enums\LoyalityPoint\LoyalityPointStatusEnum;
+use App\Enums\LoyalityPoint\LoyalityPointTypeEnum;
+use App\Enums\Purchase\OrderStatusEnum;
 use App\Enums\Purchase\OrderTypeEnum;
 use App\Enums\Purchase\PaymentStatusEnum;
 use App\Models\LoyalityPoint;
@@ -46,6 +50,60 @@ class Order extends Model
         'order_type' => OrderTypeEnum::class,
         'payment_status' => PaymentStatusEnum::class
     ];
+
+    public static function boot()
+    {
+        parent::boot();
+        static::updating(function ($order) {
+            $user = $order->user;
+            $latest_approved_loyality_points = null;
+            if ($user) {
+                $latest_approved_loyality_points = $user->latestApprovedLoyalityPoints;
+                $balance_after = $earned_points = $order->price * LoyalityPoint::LOYALITY_POINTS; #FIRST TIME DEFAULT
+            }
+
+            if ($order->status == OrderStatusEnum::PENDING) {
+                /* foreach ($order->orderItems as $order_item) {
+                    $order->assignedVendor
+                        ->vendor
+                        ->vendorProductPrices()
+                        ->where('product_variation_id', $order_item['item_variant_id'])
+                        ->increment('units_in_stock', $order_item['quantity']);
+                } */
+                $order->loyalityPoint()->delete();
+            } elseif ($order->status == OrderStatusEnum::DELIVERED) {
+                if ($user && $order->loyalityPoint()->doesntExist()) {
+                    if ($latest_approved_loyality_points) { # if previous approved loyality point exists
+                        $balance_after = $latest_approved_loyality_points->balance_after + $earned_points;
+                    }
+                    /* Log::info([
+                        $order->loyalityPoint()->doesntExist(),
+                        $user,
+                        $latest_approved_loyality_points,
+                        $balance_after,
+                    ]); */
+                    /* foreach ($order->orderItems as $order_item) {
+                        $order->assignedVendor
+                            ->vendor
+                            ->vendorProductPrices()
+                            ->where('product_variation_id', $order_item['item_variant_id'])
+                            ->decrement('units_in_stock', $order_item['quantity']);
+                    } */
+                    if ($user) {
+                        $order->loyalityPoint()->create([
+                            'user_id' => $order->user_id,
+                            'points' => $earned_points,
+                            'type' => LoyalityPointTypeEnum::EARN,
+                            'source' => LoyalityPointSourceEnum::ORDER_PURCHASE,
+                            'description' => 'loyality points earned from :' . LoyalityPointSourceEnum::ORDER_PURCHASE->value,
+                            'status' => LoyalityPointStatusEnum::APPROVED,
+                            'balance_after' => $balance_after,
+                        ]);
+                    }
+                }
+            }
+        });
+    }
 
     function orderItems()
     {
