bf0b6ebfa67a760084028ab0292602743f290d44
DESKTOP-QCFO6Q2\NIKESH <nekesmgr@gmail.com>
2025-11-25 15:37:57 +0545

    class name corrected

--- app/Http/Requests/Admin/Product/ProductUpdateRequest.php
+++ app/Http/Requests/Admin/Product/ProductUpdateRequest.php
@@ -6,7 +6,7 @@ use App\Enums\ProductUnitEnum;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
-class ProductupdateRequest extends FormRequest
+class ProductUpdateRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
