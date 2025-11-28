2ab3ea0dfa196d9f0a38af0da182c00efcf4aec8
DESKTOP-QCFO6Q2\NIKESH <nekesmgr@gmail.com>
2025-11-28 10:24:36 +0545

    product seeder 200 -> 100

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -21,7 +21,7 @@ class ProductSeeder extends Seeder
     public function run(): void
     {
         DB::transaction(function () {
-            for ($i = 1; $i <= 200; $i++) {
+            for ($i = 1; $i <= 100; $i++) {
                 $apply_discount = fake()->boolean(50);
 
                 $product = [
