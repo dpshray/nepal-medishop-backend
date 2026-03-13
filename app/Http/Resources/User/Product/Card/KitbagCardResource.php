91b331d3544b1f7640fe33100a6080a3d2adafda
Soyamstha <shrestha.soyam111@gmail.com>
2026-03-13 12:15:11 +0545

    change

--- app/Http/Resources/User/Product/Card/KitbagCardResource.php
+++ app/Http/Resources/User/Product/Card/KitbagCardResource.php
@@ -16,19 +16,21 @@ class KitbagCardResource extends JsonResource
     public function toArray(Request $request): array
     {
         // return parent::toArray($request);
-        $price=$this->variation->platform_price-($this->variation->platform_price*($this->product->discount_percent/100));
+        $price = $this->variation->platform_price - ($this->variation->platform_price * ($this->product->discount_percent / 100));
         return [
             "item_uuid" => $this->uuid,
             "item_name" => $this->product->name,
             "item_slug" => $this->product->slug,
             "brand_name" => $this->product->brand->name,
-            "variant_name" => $this->variation->name,
+            "variant_name" => $this->variation->strength,
             "variant_id" => $this->variation->id,
+            "form_type" => $this->variation->form_type,
+            "package_type" => $this->variation->package_type . ' ' . $this->variation->package_size . ' ' . $this->variation->size_unit,
             'isPrescriptionRequired' => (bool) $this->product->prescription_required,
             "image" => $this->product->getFirstMediaUrl(Product::PRODUCT_FEATURE),
-            "quantity" => (integer) $this->quantity,
-            "price"=>(float)$price,
-            "previous_price" =>$this->product->discount_percent !== null? (float) $this->variation->platform_price:null,
+            "quantity" => (int) $this->quantity,
+            "price" => (float)$price,
+            "previous_price" => $this->product->discount_percent !== null ? (float) $this->variation->platform_price : null,
             "subtotal" => (float) ($this->quantity * $price),
         ];
     }
